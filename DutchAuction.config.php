5392d3ecca5d8122568e10c6e3d65971e86e5227
Martijn de Geus <martijn@side-shore.nl>
2020-02-26 22:23:38 +0100

    first commit

---
+++ DutchAuction.config.php
@@ -0,0 +1,35 @@
+<?php namespace ProcessWire;
+
+$tmpTemplates = wire('templates');
+
+foreach($tmpTemplates as $template) { // exclude system templates
+  if($template->flags & Template::flagSystem) continue;
+  $templates[] = $template;
+    }
+
+$config = array(
+  'auctionLength' => array(
+    'type' => 'integer',
+    'label' => __('Length of the auction'),
+    'description' => __('Length of the auction in seconds.'), 
+    'value' => '1209600',
+    'required' => true,
+    'notes' => __('The lenght of the Auction, and the size of the steps defines the price difference per step. Defaults to 1209600 (14 days)')
+  ),
+  'stepSize' => array(
+    'type' => 'integer',
+    'label' => __('Step size'),
+    'description' => __('Calculate the price every n seconds'),
+    'value' => '1',
+    'required' => true, 
+    'notes' => __('The lenght of the Auction, and the size of the steps defines the price difference per step. Defaults to 1 second')
+  ),
+  'auctionTemplates' => array(
+    'type' => 'InputfieldAsmSelect',
+    'options' => $templates,
+    'label' => __('Auction templates'),
+    'description' => __('Select the templates for the auctions'),
+    'value' => '1',
+    'required' => true
+  )
+);
---
+++ DutchAuction.module.php
@@ -0,0 +1,195 @@
+<?php namespace ProcessWire;
+
+/**
+ * ProcessWire 'Hello world' demonstration module
+ *
+ * Demonstrates the Module interface and how to add hooks.
+ * 
+ * See README file for further links regarding module development.
+ * 
+ * This file is licensed under the MIT license
+ * https://processwire.com/about/license/mit/
+ * 
+ * ProcessWire 3.x, Copyright 2016 by Ryan Cramer
+ * https://processwire.com
+ *
+ */
+
+class DutchAuction extends WireData implements Module {
+
+	/**
+	 * Initialize the module
+	 *
+	 * ProcessWire calls this when the module is loaded. For 'autoload' modules, this will be called
+	 * when ProcessWire's API is ready. As a result, this is a good place to attach hooks. 
+	 *
+	 */
+	public function init() {
+		// Default auction length to two weeks
+		if(!$this->auctionLength) $this->auctionLength = 1209600;
+		// default step size
+		if(!$this->stepSize) $this->stepSize = 1;
+		// default template if non is set
+		if(!$this->auctionTemplates) $this->auctionTemplates = 'auction';
+
+		// Add lazycron hook to update the currentprices
+		$this->addHook('LazyCron::every5Minutes', $this, 'updatePrices');
+		// Add lazycron hook to archive expired auctions
+		$this->addHook('LazyCron::every5Minutes', $this, 'archiveAuctions');
+	}
+
+	public function ready () {
+	}
+
+	/**
+	 * Hoeveel Steps zijn er verlopen sinds tijdpublish
+	 * 
+	 * @param Date $dtstart
+	 * @param stepSize $stepsize
+	 */
+	protected function stepsPassed($dtstart) {
+		$dtstart = new \DateTime( $dtstart );
+		$now = new \DateTime();
+		$stepSize = $this->stepSize;
+
+		$interval = $now->getTimestamp() - $dtstart->getTimestamp();
+		$stepsPassed = $interval/$stepSize;
+
+		return $stepsPassed;
+	}
+
+
+	/**
+	 *  Wat 'Kost' 1 Step
+	 * 
+	 * @param maxPrice $maxPrice
+	 * @param minPrice $minPrice
+	 */
+	protected function stepPrice($maxPrice, $minPrice) {
+		if(isset($maxPrice, $minPrice) && $maxPrice > $minPrice){
+			$numSteps = $this->auctionLength/$this->stepSize;
+			$stepPrice = ($maxPrice-$minPrice)/$numSteps;
+		} else {
+			$stepPrice = NULL;
+		}
+			return $stepPrice;
+	}
+
+
+	/**
+	 *  Wat is de huidige prijs
+	 * 		$currentprice = $maxprice-($stepsPassed*$stepPrice)
+	 * 
+	 * @param date_published $date_published
+	 * @param maxPrice $maxPrice
+	 * @param minPrice $minPrice
+	 */	
+	public function currentPrice($date_published, $maxPrice, $minPrice) {
+		if(!isset($date_published, $maxPrice, $minPrice)) {
+			return NULL;
+		}
+		$stepsPassed = $this->stepsPassed($date_published);
+		$stepPrice = $this->stepPrice($maxPrice, $minPrice);
+
+		if($stepPrice) {
+			$currentPrice = $maxPrice-($stepsPassed*$stepPrice);
+		} else {
+			$currentPrice = $maxPrice;
+		}
+		// Never return a negative price
+		if ($currentPrice >= $minPrice){
+			return $currentPrice;
+		}else{
+			return $minPrice;
+		}
+		
+	}
+
+	// updates the prices by cron every 5 minutes
+	protected function updatePrices(HookEvent $e) {
+		$seconds = $e->arguments[0];
+		wire('log')->save('dutchauction', 'updatePrices triggered by cron (' . $seconds .' seconds  sinds last run...)');
+		$auctioncouter = 0;
+		foreach(pages()->find("template=$this->auctionTemplates, parent=/auctions/") as $auction):
+			$date_published = gmdate('Y-m-d\TH:i:s', $auction->published);
+			$currentPrice = $this->currentPrice($date_published, $auction->maxprice, $auction->minprice);
+			$auction->currentprice = $currentPrice;
+			$auction->of(false);
+			$auction->save();
+			$auction->of(true);
+			$auctioncouter ++;
+		endforeach;
+		wire('log')->save('dutchauction', 'Updated ' . $auctioncouter .' auction prices');
+	}
+
+	// Archive auctions older than auctionLength
+	protected function archiveAuctions(HookEvent $e) {
+		$seconds = $e->arguments[0];
+		wire('log')->save('dutchauction', 'archiveAuctions triggered by cron (' . $seconds .' seconds  sinds last run...)');
+		$auctioncouter = 0;
+
+		$now = new \DateTime();
+		$dtArchive = $now->getTimestamp() - $this->auctionLength;
+
+		$archiveParent = '/archive/';
+		$archiveParentYear = $now->format('Y');
+		$archiveParentMonth = $now->format('m');
+		$archiveParentDay = $now->format('d');
+
+		// Create the archive structure. Nested y->M->D
+		$archiveParentNow = pages()->get($archiveParent);
+		if (!$archiveParentNow->id ) {
+			$archiveParentNow = wire(new Page());
+			$archiveParentNow->template = 'auctionoverview';
+			$archiveParentNow->parent = wire('pages')->get('/');
+			$archiveParentNow->title = 'Archive';
+			$archiveParentNow->save();
+			wire('log')->save('dutchauction', 'Created ' . $archiveParentNow .' page');
+		}
+
+		$archiveParentYearNow = pages()->get("parent=$archiveParent, title=$archiveParentYear");
+		if (!$archiveParentYearNow->id ) {
+			$archiveParentYearNow = wire(new Page());
+			$archiveParentYearNow->template = 'auctionoverview';
+			$archiveParentYearNow->parent = $archiveParentNow;
+			$archiveParentYearNow->title = $archiveParentYear;
+			$archiveParentYearNow->save();
+			wire('log')->save('dutchauction', 'Created ' . $archiveParentYearNow .' page');
+		}
+
+		$archiveParentMonthNow = pages()->get("parent=$archiveParentYearNow, title=$archiveParentMonth");
+		if (!$archiveParentMonthNow->id ) {
+			$archiveParentMonthNow = wire(new Page());
+			$archiveParentMonthNow->template = 'auctionoverview';
+			$archiveParentMonthNow->parent = $archiveParentYearNow;
+			$archiveParentMonthNow->title = $archiveParentMonth;
+			$archiveParentMonthNow->save();
+			wire('log')->save('dutchauction', 'Created ' . $archiveParentMonthNow .' page');
+		}
+	
+		$archiveParentDayNow = pages()->get("parent=$archiveParentMonthNow, title=$archiveParentDay");
+		if (!$archiveParentDayNow->id ) {
+			$archiveParentDayNow = wire(new Page());
+			$archiveParentDayNow->template = 'auctionoverview';
+			$archiveParentDayNow->parent = $archiveParentMonthNow;
+			$archiveParentDayNow->title = $archiveParentDay;
+			$archiveParentDayNow->save();
+			wire('log')->save('dutchauction', 'Created ' . $archiveParentDayNow .' page');
+		}
+
+		
+		foreach(pages()->find("template=auction, parent=/auctions/") as $auction):
+			if($auction->published < $dtArchive){
+				$auction->parent = $archiveParentDayNow;
+				$auction->addStatus(Page::statusUnpublished);;
+				$auction->of(false);
+				$auction->save();
+				$auction->of(true);
+				$auctioncouter ++;
+			}
+		endforeach;
+		wire('log')->save('dutchauction', 'Archived ' . $auctioncouter .' auctions');
+	}
+
+
+}
